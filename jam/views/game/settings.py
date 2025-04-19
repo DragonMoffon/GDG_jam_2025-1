98981eab4d8a5ea75a5f5e275ad115abc8cf8e56
DragonMoffon <happydog.aja@gmail.com>
2025-04-19 17:00:11 +1200

    update size of settings frame

--- jam/views/game/settings.py
+++ jam/views/game/settings.py
@@ -5,5 +5,5 @@ from resources import style
 class SettingsFrame(Frame):
 
     def __init__(self, position: tuple[float, float], height: float, show_body: bool = False, show_shadow: bool = True):
-        size = (300, height)
+        size = (450, height)
         super().__init__('SETTINGS', 0.0, position, size, show_body, show_shadow, anchor_top=False)
