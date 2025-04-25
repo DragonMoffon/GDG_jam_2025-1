4eefce1a4e095570ef2aea67ea72656105be858e
A J Andrews <happydog.aja@gmail.com>
2025-04-25 13:29:21 +1200

    Add editor icons to style.py

--- resources/style.py
+++ resources/style.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass
 import tomllib
 from arcade import load_font
 from arcade.types import RGBA255
+from pyglet.image import ImageData, load as load_texture
 
 import resources.styles as styles
 
@@ -22,34 +23,6 @@ class Colors:
 
 
 @dataclass
-class EditorColors:
-    shadow: RGBA255
-    block: RGBA255
-    background: RGBA255
-    accent: RGBA255
-    connection: RGBA255
-    select: RGBA255
-
-
-@dataclass
-class EditorBlock:
-    radius: float
-    header: float
-    footer: float
-    select: float
-
-
-@dataclass
-class Editor:
-    point_radius: float
-    line_thickness: float
-    padding: float
-    drop_x: int
-    drop_y: int
-    colors: EditorColors
-    block: EditorBlock
-
-@dataclass
 class Format:
     point_radius: float
     corner_radius: float
@@ -61,6 +34,7 @@ class Format:
     drop_x: float
     drop_y: float
 
+
 @dataclass
 class Panels:
     settings_tag: Path
@@ -69,9 +43,21 @@ class Panels:
     info_tag: Path
     panel_speed: float
 
+
 @dataclass
 class Editor:
     background: Path
+
+    node_inactive: ImageData
+    node_active: ImageData
+    branch_inactive: ImageData
+    branch_active: ImageData
+    variable: ImageData
+    defintiion: ImageData
+    dropdown: ImageData
+    check_inactive: ImageData
+    check_active: ImageData
+
 
 @dataclass
 class Game:
@@ -79,16 +65,19 @@ class Game:
     panels: Panels
     editor: Editor
 
+
 @dataclass
 class TextFormat:
     name: str
     path: str
     size: float
 
+
 @dataclass
 class Text:
     normal: TextFormat
     header: TextFormat
+
 
 class Style:
 
@@ -107,25 +96,33 @@ class Style:
         load_font(source / self.text.normal.path)
         load_font(source / self.text.header.path)
 
-        self.format = Format(
-            **self._raw['Format']
-        )
+        self.format = Format(**self._raw["Format"])
 
-        panel_data = self._raw['Game']['Panels']
+        panel_data = self._raw["Game"]["Panels"]
+        editor_data = self._raw["Game"]["Editor"]
         self.game = Game(
-            source / self._raw['Game']['background'],
+            source / self._raw["Game"]["background"],
             Panels(
-                settings_tag=source / panel_data['settings_tag'],
-                editor_tag=source / panel_data['editor_tag'],
-                comms_tag=source / panel_data['comms_tag'],
-                info_tag=source / panel_data['info_tag'],
-                panel_speed=panel_data['panel_speed']
+                settings_tag=source / panel_data["settings_tag"],
+                editor_tag=source / panel_data["editor_tag"],
+                comms_tag=source / panel_data["comms_tag"],
+                info_tag=source / panel_data["info_tag"],
+                panel_speed=panel_data["panel_speed"],
             ),
             Editor(
-                source / self._raw['Game']['Editor']['background']
-            )
+                source / editor_data["background"],
+                load_texture(source / editor_data["node_inactive"]),
+                load_texture(source / editor_data["node_active"]),
+                load_texture(source / editor_data["branch_inactive"]),
+                load_texture(source / editor_data["branch_active"]),
+                load_texture(source / editor_data["variable"]),
+                load_texture(source / editor_data["definition"]),
+                load_texture(source / editor_data["dropdown"]),
+                load_texture(source / editor_data["check_inactive"]),
+                load_texture(source / editor_data["check_active"]),
+            ),
         )
 
 
-with path(styles, 'base') as pth:
+with path(styles, "base") as pth:
     STYLE = Style(pth)
