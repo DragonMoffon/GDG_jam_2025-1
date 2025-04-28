dd5a88e316a7cd2b8675799f7b2a3ab1123397cf
A J Andrews <happydog.aja@gmail.com>
2025-04-28 12:33:10 +1200

    cleanup of game view

--- jam/views/game/__init__.py
+++ jam/views/game/__init__.py
@@ -3,20 +3,21 @@ from enum import Enum
 from pyglet.graphics import Batch
 from arcade.future.background import Background
 
+from resources import style
+
 from jam.view import View
 from jam.gui.frame import Frame, FrameController
-from jam.input import inputs, Button, Axis
-
+from jam.input import Button, Axis
 
 from .editor import EditorFrame
 from .settings import SettingsFrame
 
-from resources import style
 
 class FrameAnimationMode(Enum):
     NONE = 0
     SHOW = 1
     HIDE = 2
+
 
 class GameView(View):
 
@@ -29,35 +30,56 @@ class GameView(View):
         self._editor_frame = EditorFrame(0.0, (self.width, 0.0), 720)
         self._editor_frame.connect_renderer(self._batch)
 
-        self._info_frame = Frame('INFO', self._editor_frame.tag_height + 2 * style.format.padding, (self.width, 0.0), (450, 720))
+        self._info_frame = Frame(
+            "INFO",
+            self._editor_frame.tag_height + 2 * style.format.padding,
+            (self.width, 0.0),
+            (450, 720),
+        )
         self._info_frame.connect_renderer(self._batch)
 
-        self._comms_frame = Frame('COMMS', self._editor_frame.tag_height + self._info_frame.tag_height + 3 * style.format.padding, (self.width, 0.0), (450, 720))
+        self._comms_frame = Frame(
+            "COMMS",
+            self._editor_frame.tag_height
+            + self._info_frame.tag_height
+            + 3 * style.format.padding,
+            (self.width, 0.0),
+            (450, 720),
+        )
         self._comms_frame.connect_renderer(self._batch)
 
         self._setting_frame = SettingsFrame((self.width, 0.0), 720)
         self._setting_frame.connect_renderer(self._batch)
 
-        self._frame_controller = FrameController((self._editor_frame, self._setting_frame, self._info_frame, self._comms_frame), (self.width, 0.0))
+        self._frame_controller = FrameController(
+            (
+                self._editor_frame,
+                self._setting_frame,
+                self._info_frame,
+                self._comms_frame,
+            ),
+            (self.width, 0.0),
+        )
 
-
-    def on_draw(self) -> bool | None:
+    def on_draw(self) -> None:
         self.clear()
         self._background_image.draw()
         self._frame_controller.on_draw()
         self._batch.draw()
 
-    def on_update(self, delta_time: float) -> bool | None:
+    def on_update(self, delta_time: float) -> None:
         self._frame_controller.on_update(delta_time)
 
-    def on_input(self, input: Button, modifiers: int, pressed: bool) -> bool | None:
+    def on_input(self, input: Button, modifiers: int, pressed: bool) -> None:
         self._frame_controller.on_input(input, modifiers, pressed)
 
-    def on_axis_change(self, axis: Axis, value_1: float, value_2: float):
+    def on_axis_change(self, axis: Axis, value_1: float, value_2: float) -> None:
         self._frame_controller.on_axis_change(axis, value_1, value_2)
 
-    def on_cursor_motion(self, x: float, y: float, dx: float, dy: float) -> bool | None:
+    def on_cursor_motion(self, x: float, y: float, dx: float, dy: float) -> None:
         self._frame_controller.on_cursor_motion(x, y, dx, dy)
 
-    def on_cursor_scroll(self, x: float, y: float, scroll_x: float, scroll_y: float) -> bool | None:
+    def on_cursor_scroll(
+        self, x: float, y: float, scroll_x: float, scroll_y: float
+    ) -> None:
         self._frame_controller.on_cursor_scroll(x, y, scroll_x, scroll_y)
