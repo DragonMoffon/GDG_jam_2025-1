b7a920a77dccb490e82fce3338c7602e9edeb6e6
A J Andrews <happydog.aja@gmail.com>
2025-04-28 13:41:36 +1200

    Added debris parallax and moved imgs

--- jam/views/game/__init__.py
+++ jam/views/game/__init__.py
@@ -1,9 +1,11 @@
-from enum import Enum
+from math import cos, sin, tau
 
 from pyglet.graphics import Batch
-from arcade.future.background import Background
+from arcade.clock import GLOBAL_CLOCK
+from arcade.future.background import Background, BackgroundGroup
 
 from resources import style
+from resources.style import FloatMotionMode
 
 from jam.view import View
 from jam.gui.frame import Frame, FrameController
@@ -13,18 +15,49 @@ from .editor import EditorFrame
 from .settings import SettingsFrame
 
 
-class FrameAnimationMode(Enum):
-    NONE = 0
-    SHOW = 1
-    HIDE = 2
+class ParallaxBackground:
+
+    def __init__(self):
+        self._base: Background = Background.from_file(
+            style.game.background.base, style.game.background.base_offset
+        )
+        self._layers: tuple[Background, ...] = tuple(
+            Background.from_file(floating.src, floating.offset)
+            for floating in style.game.background.layers
+        )
+        self._background = BackgroundGroup([self._base, *self._layers])
+
+    def cursor_motion(self, x: float, y: float, dx: float, dy: float) -> None:
+        for idx, layer in enumerate(self._layers):
+            data = style.game.background.layers[idx]
+            origin = (
+                data.foci[0] * layer.texture.texture.width,
+                data.foci[1] * layer.texture.texture.height,
+            )
+            shift = (x - origin[0]) / (data.depth), (y - origin[1]) / (data.depth)
+            layer.pos = shift
+
+    def update(self) -> None:
+        t = GLOBAL_CLOCK.time
+        for idx, layer in enumerate(self._layers):
+            data = style.game.background.layers[idx]
+            fraction = (t + data.sync) % data.rate / data.rate
+            match data.mode:
+                case FloatMotionMode.CIRCLE:
+                    x = data.scale * cos(fraction * tau)
+                    y = data.scale * sin(fraction * tau)
+                    layer.texture.offset = (x, y)
+
+    def draw(self) -> None:
+        self._background.draw()
 
 
 class GameView(View):
 
     def __init__(self) -> None:
         View.__init__(self)
-        self.background_color = style.colors.dark
-        self._background_image = Background.from_file(style.game.background)
+        self.background_color = style.game.background.colour
+        self._background = ParallaxBackground()
         self._batch = Batch()
 
         self._editor_frame = EditorFrame(0.0, (self.width, 0.0), 720)
@@ -63,11 +96,12 @@ class GameView(View):
 
     def on_draw(self) -> None:
         self.clear()
-        self._background_image.draw()
+        self._background.draw()
         self._frame_controller.on_draw()
         self._batch.draw()
 
     def on_update(self, delta_time: float) -> None:
+        self._background.update()
         self._frame_controller.on_update(delta_time)
 
     def on_input(self, input: Button, modifiers: int, pressed: bool) -> None:
@@ -77,6 +111,7 @@ class GameView(View):
         self._frame_controller.on_axis_change(axis, value_1, value_2)
 
     def on_cursor_motion(self, x: float, y: float, dx: float, dy: float) -> None:
+        self._background.cursor_motion(x, y, dx, dy)
         self._frame_controller.on_cursor_motion(x, y, dx, dy)
 
     def on_cursor_scroll(
