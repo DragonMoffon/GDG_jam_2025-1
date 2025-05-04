d92a9b70bc4373a600c5f425f5295838dc644ae5
A J Andrews <happydog.aja@gmail.com>
2025-05-05 11:56:33 +1200

    relative import tsk tsk

--- jam/__main__.py
+++ jam/__main__.py
@@ -12,7 +12,7 @@
 #   nuitka-project: --windows-console-mode=disable
 #   nuitka-project: --windows-icon-from-ico=icon.png
 
-from .main import main
+from jam.main import main
 
 if __name__ == "__main__":
     main()
