2c6414b0c6076d3b6efdd6ea602602898ceb4a62
A J Andrews <happydog.aja@gmail.com>
2025-05-06 17:40:23 +1200

    can now choose save file, and sped up transition when continuing

--- jam/views/menu/__init__.py
+++ jam/views/menu/__init__.py
@@ -9,10 +9,14 @@ from jam.gui.core import Gui
 from jam.gui.util import PopupAction, SelectionPopup
 from jam.context import context
 from jam.views.game import GameView
-from jam.input import inputs
+from jam.input import inputs, Button
 
 
 class MainMenuView(View):
+    SLOW_FADE = 4.0
+    SLOW_TRANSITION = 6.0
+    FAST_FADE = 1.0
+    FAST_TRANSITION = 1.5
 
     def __init__(self):
         View.__init__(self)
@@ -23,11 +27,17 @@ class MainMenuView(View):
         self._gui = Gui(self.window.default_camera)
         new_save = PopupAction("New Game", self.new_save)
         cont_save = PopupAction("Continue", self.continue_save)
+        saves = (
+            PopupAction(f"Continue: {name}", self.pick_save, name)
+            for name in context.get_save_names()[::-1]
+        )
         self._popup = SelectionPopup(
-            (new_save, cont_save), (self.center_x, self.center_y)
+            (new_save, *saves, cont_save), (self.center_x, self.center_y)
         )
         self._gui.add_element(self._popup)
         self._fade_out: bool = False
+        self._speed: float = self.SLOW_FADE
+        self._tranistion: float = self.SLOW_TRANSITION
         self._timer: float = 0.0
 
     def new_save(self) -> None:
@@ -39,14 +49,16 @@ class MainMenuView(View):
     def continue_save(self) -> None:
         self._fade_out = True
         self._timer = self.window.time
+        self._speed = self.FAST_FADE
+        self._tranistion = self.FAST_TRANSITION
         context.choose_first_save()
-        style.audio.crash.play()
 
     def pick_save(self, name: str) -> None:
         self._fade_out = True
         self._timer = self.window.time
+        self._speed = self.FAST_FADE
+        self._tranistion = self.FAST_TRANSITION
         context.choose_save(name)
-        style.audio.crash.play()
 
     def on_cursor_motion(self, x, y, dx, dy) -> None:
         l = self._popup.get_hovered_item((x, y))
@@ -54,18 +66,20 @@ class MainMenuView(View):
             self._popup.highlight_action(l)
         self._background.cursor_motion(x, y, dx, dy)
 
-    def on_input(self, button, modifiers, pressed) -> None:
+    def on_input(self, button: Button, modifiers: int, pressed: bool) -> None:
         if button == inputs.PRIMARY_CLICK and pressed:
             l = self._popup.get_hovered_item(inputs.cursor)
             if l is not None:
                 self._popup.actions[l]()
 
-    def on_draw(self):
+    def on_draw(self) -> None:
         self.clear()
         self._background.draw()
         self._gui.draw()
         if self._fade_out:
-            fraction = (self.window.time - self._timer) / 4.0  # fade for five seconds
+            fraction = (
+                self.window.time - self._timer
+            ) / self._speed  # fade for five seconds
             amount = max(0.0, min(1.0, (1 - (1 - fraction) ** 3)))
             draw_rect_filled(self.window.rect, (0, 0, 0, int(255 * amount)))
             self._logo.color = (255, 255, 255, int(255 * amount))
@@ -76,5 +90,5 @@ class MainMenuView(View):
         if not self._fade_out:
             return
 
-        if self.window.time - self._timer > 6.0:
+        if self.window.time - self._timer > self._tranistion:
             self.window.show_view(GameView())
