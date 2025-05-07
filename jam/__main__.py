ed2d37260c0abf4cdcd2c051c7f4d36facde0349
DragonMoffon <happydog.aja@gmail.com>
2025-05-08 01:10:01 +1200

    add icon and remove vscode folder from repo (sorry digi)

--- jam/__main__.py
+++ jam/__main__.py
@@ -1,16 +1,17 @@
 # nuitka-project: --include-package-data=resources
+# nuitka-project: --include-module=arcade.future
 # nuitka-project: --force-stderr-spec=err.txt
 # nuitka-project: --report=compilation-report.xml
 # nuitka-project: --standalone
 # nuitka-project: --product-name="Station"
-# nuitka-project: --product-version="0.1.0.0"
-# nuitka-project: --file-description=""
+# nuitka-project: --product-version="0.2.0.1"
+# nuitka-project: --file-description="Created by DigiDuncan and DragonMoffon"
 # nuitka-project-if: {OS} == "Darwin":
 #   nuitka-project: --macos-create-app-bundle
-#   nuitka-project: --macos-app-icon=icon.png
+#   nuitka-project: --macos-app-icon=icon.ico
 # nuitka-project-if: {OS} == "Windows":
 #   nuitka-project: --windows-console-mode=disable
-#   nuitka-project: --windows-icon-from-ico=icon.png
+#   nuitka-project: --windows-icon-from-ico=icon.ico
 
 from jam.main import main
 
