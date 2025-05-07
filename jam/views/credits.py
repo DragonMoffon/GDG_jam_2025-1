3a7214eabbb0f28cc07d21fe8d879b6a86136d9c
A J Andrews <happydog.aja@gmail.com>
2025-05-08 10:19:07 +1200

    credits scroll

--- jam/views/credits.py
+++ jam/views/credits.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+from arcade import Camera2D
 from resources import style
 
 from jam.graphics.format_label import FLabel
@@ -13,7 +14,7 @@ def has_credits() -> bool:
 
 
 def get_credits() -> str:
-    with open(Path("CREDITS.md")) as fp:
+    with open(Path("CREDITS.md"), encoding="utf-8") as fp:
         return fp.read()
 
 
@@ -24,24 +25,34 @@ class CreditsView(View):
         self.back: View = back
 
         self._background = ParallaxBackground(style.menu.background)
+        self._camera = Camera2D(projection=self.window.rect)
         self._text = FLabel(
             get_credits(),
-            self.center_x,
-            self.center_y + self.height / 3,
+            x=self.center_x,
+            y=self.center_y,
+            multiline=True,
+            width=self.width * 2.0 / 3.0,
             anchor_x="center",
             anchor_y="center",
+            align="center",
+            font_name=style.text.normal.name,
+            font_size=style.text.normal.size,
         )
 
     def on_draw(self) -> None:
         self.clear()
         self._background.draw()
-        self._text.draw()
+        with self._camera.activate():
+            self._text.draw()
 
     def on_update(self, delta_time: float) -> None:
         self._background.update()
 
     def on_cursor_motion(self, x: float, y: float, dx: float, dy: float) -> None:
         self._background.cursor_motion(x, y, dx, dy)
+
+        h = self._text.content_height * (y / self.height - 0.5)
+        self._camera.position = (0.0, h)
 
     def on_input(self, button: Button, modifiers: int, pressed: bool) -> None:
         if pressed:
