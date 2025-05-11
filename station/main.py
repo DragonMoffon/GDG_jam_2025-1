d930a2e4b95552ad655c8fec5534dd4180e94aa9
DragonMoffon <happydog.aja@gmail.com>
2025-05-11 18:34:34 +1200

    catch import errors

--- station/main.py
+++ station/main.py
@@ -1,12 +1,21 @@
-import pyglet
-from pyglet.gl import GL_LINEAR
+try:
+    import pyglet
+    from pyglet.gl import GL_LINEAR
 
-pyglet.image.Texture.default_min_filter = GL_LINEAR
-pyglet.image.Texture.default_mag_filter = GL_LINEAR
+    pyglet.image.Texture.default_min_filter = GL_LINEAR
+    pyglet.image.Texture.default_mag_filter = GL_LINEAR
 
-from .context import context
-from .application import Window
-from .views.menu import MainMenuView
+    from .context import context
+    from .application import Window
+    from .views.menu import MainMenuView
+except (Exception, KeyboardInterrupt) as e:
+    from datetime import datetime
+    import traceback
+    from uuid import uuid4
+    crash_time = datetime.now().strftime("%Y-%m-%d %H-%M")
+    with open(f"early-crash-{crash_time}_{uuid4().hex}.txt", "w", encoding="utf-8") as fp:
+        fp.write("".join(traceback.format_exception(e)))
+    raise e from e
 
 
 def main() -> None:
